59648d2b7c0de83ef4e4e9ac59b1e7f46f1369a7
kurage <ohashih@ns.aucnet.co.jp>
2025-04-16 23:35:22 +0900

    instance

---
+++ app/base_class.py
@@ -0,0 +1,14 @@
+from typing import Any
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import as_declarative
+
+
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+
+    # Generate __tablename__ automatically
+    @declared_attr
+    def __tableanme__(cls) -> str:
+        return cls.__name__.lower()
